ca45b41cf7b06c9085b3a6af86609c781702c578
fleoren <mfalcalad@gmail.com>
2018-01-04 20:15:56 -0600

    Las queries de insercion tenian parentesis de mas

--- code/model/prepare_experiment_information.py
+++ code/model/prepare_experiment_information.py
@@ -22,18 +22,18 @@ def insert(name, columns, data):
         cur = conn.cursor()
         # Depending on the table, single ur multiple insertions are needed
         if name == "experiments" or name == "world_parameters":
-            sql_query_empty = """INSERT INTO policy_iteration.%s (%s) VALUES (%s);"""
+            sql_query_empty = """INSERT INTO policy_iteration.%s (%s) VALUES %s;"""
             # Create the final query to be executed
             sql_query = sql_query_empty % (name, columns, data)
             # Execute query
             cur.execute(sql_query)
         elif name == "agent_parameters":
-            sql_query_semi = "INSERT INTO " + name + "(" + columns + ") " + """VALUES (%(experiment_id)s, %(agent)s, %(initial_inventory)s, %(buying_price)s, %(selling_price)s);"""
+            sql_query_semi = "INSERT INTO " + name + "(" + columns + ") " + """VALUES %(experiment_id)s, %(agent)s, %(initial_inventory)s, %(buying_price)s, %(selling_price)s;"""
             # Execute query: for many need a mapping
             # https://stackoverflow.com/questions/8134602/psycopg2-insert-multiple-rows-with-one-query
             cur.executemany(sql_query_semi, data)
         elif name == "experiment_results":
-            sql_query_semi = "INSERT INTO " + name + "(" + columns + ") " + """VALUES (%(experiment_id)s, %(agent)s, %(best_payout)s, %(best_policy)s, %(historic_payout)s, %(policy_inventory)s, %(total_money)s);"""
+            sql_query_semi = "INSERT INTO " + name + "(" + columns + ") " + """VALUES %(experiment_id)s, %(agent)s, %(best_payout)s, %(best_policy)s, %(historic_payout)s, %(policy_inventory)s, %(total_money)s;"""
             # Execute query: for many need a mapping
             cur.executemany(sql_query_semi, data)
         # commit the changes to the database
