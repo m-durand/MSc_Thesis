9eafbeedbedd9e87e4d64db2544d90dcac9777b6
Fernanda Alcala <mfalcalad@gmail.com>
2018-07-12 15:42:49 +0200

    small cahnges such as length of one variable and typos on structure

--- code/etl/create_database_schema_and_tables.sql
+++ code/etl/create_database_schema_and_tables.sql
@@ -1,3 +1,8 @@
+-- Create the necessary user - best to keep things neat and tidy!
+-- password to be provided separately
+-- CREATE ROLE experiments WITH LOGIN PASSWORD '';
+-- ALTER ROLE experiments CREATEDB;
+
 -- Database: reinforcement_learning
 
 -- DROP DATABASE reinforcement_learning;
@@ -6,8 +11,8 @@ CREATE DATABASE reinforcement_learning
     WITH 
     OWNER = experiments
     ENCODING = 'UTF8'
-    LC_COLLATE = 'English_United States.1252'
-    LC_CTYPE = 'English_United States.1252'
+    -- LC_COLLATE = 'English_United States.1252'
+    -- LC_CTYPE = 'English_United States.1252'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
@@ -24,7 +29,7 @@ CREATE SCHEMA policy_iteration
 
 CREATE TABLE policy_iteration.agent_parameters
 (
-    experiment_id character(32) COLLATE pg_catalog."default" NOT NULL,
+    experiment_id character(56) COLLATE pg_catalog."default" NOT NULL,
     agent character varying(1) COLLATE pg_catalog."default" NOT NULL,
     initial_inventory integer NOT NULL,
     buying_price integer NOT NULL,
@@ -45,7 +50,7 @@ ALTER TABLE policy_iteration.agent_parameters
 
 CREATE TABLE policy_iteration.experiment_results
 (
-    experiment_id character(32) COLLATE pg_catalog."default" NOT NULL,
+    experiment_id character(56) COLLATE pg_catalog."default" NOT NULL,
     agent character(20) COLLATE pg_catalog."default" NOT NULL,
     best_payout character varying COLLATE pg_catalog."default" NOT NULL,
     best_policy character varying COLLATE pg_catalog."default" NOT NULL,
@@ -68,7 +73,7 @@ ALTER TABLE policy_iteration.experiment_results
 
 CREATE TABLE policy_iteration.experiments
 (
-    experiment_id character(32) COLLATE pg_catalog."default" NOT NULL,
+    experiment_id character(56) COLLATE pg_catalog."default" NOT NULL,
     datetime timestamp without time zone NOT NULL,
     total_epochs integer NOT NULL,
     warmstart_proportion double precision NOT NULL,
@@ -82,25 +87,25 @@ WITH (
 TABLESPACE pg_default;
 
 ALTER TABLE policy_iteration.experiments
-    OWNER to postgres;
+    OWNER to experiments;
 
 -- Table: policy_iteration.world_parameters
 
 -- DROP TABLE policy_iteration.world_parameters;
 
 CREATE TABLE policy_iteration.world_parameters
-(
-    experiment_id character(32) COLLATE pg_catalog."default" NOT NULL,
-    max_demand integer NOT NULL,
-    customer_demand character varying COLLATE pg_catalog."default" NOT NULL,
-    fields_supply character varying COLLATE pg_catalog."default" NOT NULL,
-    warehouse_price integer NOT NULL,
-    CONSTRAINT world_parameters_pkey PRIMARY KEY (experiment_id)
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
+    (
+        experiment_id character(56) COLLATE pg_catalog."default" NOT NULL,
+        max_demand integer NOT NULL,
+        customer_demand character varying COLLATE pg_catalog."default" NOT NULL,
+        fields_supply character varying COLLATE pg_catalog."default" NOT NULL,
+        warehouse_price integer NOT NULL,
+        CONSTRAINT world_parameters_pkey PRIMARY KEY (experiment_id)
+    )
+    WITH (
+        OIDS = FALSE
+    )
+    TABLESPACE pg_default;
 
 ALTER TABLE policy_iteration.world_parameters
     OWNER to experiments;
