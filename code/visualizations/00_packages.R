a4866971f4a00b3fef5f933eb26389d808ddcec5
fleoren <mfalcalad@gmail.com>
2021-07-04 01:22:08 -0500

    changed colours for some visualizations to enhance them

--- code/visualizations/00_packages.R
+++ code/visualizations/00_packages.R
@@ -23,12 +23,12 @@ sapply(.packages, require, character.only = TRUE)
 #Create a custom color scale for agents - lets use throughout the whole doc -----
 
 # english scale
-agent_colors_en <- c("#F60027", "#FDB34B", "#F76B50", "#006D6F")
+agent_colors_en <- c("#3484BB", "#FF8E2B", "#4AA334", "#D62627")
 # c("#FE0068", "#F60027", "#FDB34B", "#F76B50", "#006D6F", "#6CAE3C") # if it contains customer and fields
 names(agent_colors_en) <- c("Retail", "Wholesale", "Factory", "Regional_Warehouse")
 agents_colors_en <- scale_colour_manual(name = "Agent",values = agent_colors_en)
 # english scale
-agent_colors_es <- c("#F60027", "#FDB34B", "#F76B50", "#006D6F")
+agent_colors_es <- c("#3484BB", "#FF8E2B", "#4AA334", "#D62627")
 # c("#FE0068", "#F60027", "#FDB34B", "#F76B50", "#006D6F", "#6CAE3C") # if it contains customer and fields
 names(agent_colors_es) <- c("Menudeo","Mayoreo","Almacen regional","Fabrica")
 agents_colors_es <- scale_colour_manual(name = "Agente",values = agent_colors_es)
